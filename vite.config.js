bc04aa8cab3cb1995d1487f36791e9f7f34764ec
doriskuo <doriskuo0105@gmail.com>
2025-08-29 05:12:24 +0800

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/doriskuo.github.io-dashboard-project/",
   plugins: [react(), tailwindcss()],
 });
